9f0e2370685e88e54398eaf2515200e1df1443ba
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-05-30 00:10:32 +0500

    multi-line comments with /*

--- RegEx/index.js
+++ RegEx/index.js
@@ -1,8 +1,11 @@
 //Application of regEx in frequent and structured queries (paths):
 import express from 'express';
 const app = express();
-app.get("/iphone/", (req,res)=>{
+app.get(/^\/iphone$/i, (req, res) => {
     res.send("iphone");
-})
+});
 app.listen(8000, ()=>console.log("Server started on port 8000"));
+/*
+hello my name is Mukhlisa
+*/
 
