5ff71a97c79c8a889d832172afd50b1229e2687d
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-05-31 19:21:55 +0500

    this how to write multi-function servers in express jscript

--- Callback_fns/index.js
+++ Callback_fns/index.js
@@ -1,1 +1,10 @@
 import express from 'express';
+const app = express();
+app.get("/single-cb", (req,res, next)=>{
+    console.log("GET /single-cb");
+    next()
+}, (req,res)=>{
+    res.send("Got second callback function ❤")
+})
+
+app.listen(8000, ()=>console.log("Server is running on 8000 port"))
