017d56019d7cf9869c528c048df592c2c6ebc8dd
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-06-01 18:31:23 +0500

    added some other callback functions

--- Callback_fns/index.js
+++ Callback_fns/index.js
@@ -8,9 +8,15 @@ const cb2 = (req, res, next) => {
     console.log("Second")
     next()
 }
-const cb3 = (req, res) => {
-    res.send("Third")
+const cb3 = (req, res, next) => {
+    console.log("Third")
+    next()
 }
 
-app.get("/", [cb1,cb2,cb3])
+app.get("/", [cb1,cb2, cb3], (req, res, next) => {
+    console.log("Fourth")
+    next()
+}, (req, res) => {
+    res.send("Fifth")
+    })
 app.listen(8000, ()=>console.log("Running..."))
